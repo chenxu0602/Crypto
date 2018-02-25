ed5cdec5351f8f1ca3e4a1eb983cf605a3f1cc1f
Chen Xu <chenxu@Chens-iMac.local>
2018-02-25 16:30:25 +0800

    indentation

--- data.py
+++ data.py
@@ -9,151 +9,151 @@ import numpy as np
 #pd.set_option('display.expand_frame_repr', False)
 
 def load_minute_BTCEUR():
-    return load_minute_data("Bitstamp_BTCEUR_m.csv")
+	return load_minute_data("Bitstamp_BTCEUR_m.csv")
 
 def load_minute_BTCUSD():
-    return load_minute_data("Bitstamp_BTCUSD_m.csv")
+	return load_minute_data("Bitstamp_BTCUSD_m.csv")
 
 def load_minute_ETHEUR():
-    return load_minute_data("Bitstamp_ETHEUR_m.csv")
+  	return load_minute_data("Bitstamp_ETHEUR_m.csv")
 
 def load_minute_ETHUSD():
-    return load_minute_data("Bitstamp_ETHUSD_m.csv")
+  	return load_minute_data("Bitstamp_ETHUSD_m.csv")
 
 def load_minute_LTCBTC():
-    return load_minute_data("Bitstamp_LTCBTC_m.csv")
+	return load_minute_data("Bitstamp_LTCBTC_m.csv")
 
 def load_minute_LTCEUR():
-    return load_minute_data("Bitstamp_LTCEUR_m.csv")
+	return load_minute_data("Bitstamp_LTCEUR_m.csv")
 
 def load_minute_LTCUSD():
-    return load_minute_data("Bitstamp_LTCUSD_m.csv")
+	return load_minute_data("Bitstamp_LTCUSD_m.csv")
 
 def load_minute_XRPUSD():
-    return load_minute_data("Bitstamp_XRPUSD_m.csv")
+	return load_minute_data("Bitstamp_XRPUSD_m.csv")
 
 def load_minute_data(f):
-    print("Loading file {0} ...".format(f))
-    parser = lambda date: datetime.strptime(date[:-3], "%Y-%m-%d %H:%M")
-    return pd.read_csv(f, parse_dates=[0], index_col=[0], date_parser=parser)
+	print("Loading file {0} ...".format(f))
+	parser = lambda date: datetime.strptime(date[:-3], "%Y-%m-%d %H:%M")
+	return pd.read_csv(f, parse_dates=[0], index_col=[0], date_parser=parser)
 
 def minuteOHLC(data):
-    print("Generating OHLC data ...")
-    res = defaultdict(pd.DataFrame)
-    for fld in ["Open", "High", "Low", "Close"]:
-        resDict = {}
-        for sym in sorted(data.keys()):
-            resDict[sym] = data[sym][fld]
-        res[fld] = pd.DataFrame(resDict)
-    res["Ret"] = np.log(res["Close"]).diff()
-    return res
+	print("Generating OHLC data ...")
+	res = defaultdict(pd.DataFrame)
+	for fld in ["Open", "High", "Low", "Close"]:
+		resDict = {}
+		for sym in sorted(data.keys()):
+			resDict[sym] = data[sym][fld]
+		res[fld] = pd.DataFrame(resDict)
+	res["Ret"] = np.log(res["Close"]).diff()
+	return res
 
 
 
 def processRawData(fileName="crypto-markets.csv", start="2015-01-01", dump=True):
-    if not os.path.exists(fileName):
-        print("{0} doesn't exist!".format(fileName))
-        return pd.DataFrame()
+	if not os.path.exists(fileName):
+		print("{0} doesn't exist!".format(fileName))
+		return pd.DataFrame()
 
-    print("Pocessing raw data from {0} ...".format(fileName))
+	print("Pocessing raw data from {0} ...".format(fileName))
 
-    data = pd.read_csv(fileName, parse_dates=["date"])
-    data = data[data["date"] >= start]
-    data.set_index(["slug", "date"], inplace=True)
+	data = pd.read_csv(fileName, parse_dates=["date"])
+	data = data[data["date"] >= start]
+	data.set_index(["slug", "date"], inplace=True)
 
-    frames = defaultdict(lambda: defaultdict(pd.Series))
-    for sym, df in data.groupby(level="slug"):
-        print(sym)
-        df2 = df.xs(sym, level="slug")
+	frames = defaultdict(lambda: defaultdict(pd.Series))
+	for sym, df in data.groupby(level="slug"):
+		print(sym)
+		df2 = df.xs(sym, level="slug")
 
-        for fld in data.columns:
-            series = df2[fld]
-            flt = series.groupby(level=0).filter(lambda x: len(x) > 1)
-            if len(flt) > 0: print(sym, flt)
-            frames[fld][sym] = series
+		for fld in data.columns:
+			series = df2[fld]
+			flt = series.groupby(level=0).filter(lambda x: len(x) > 1)
+			if len(flt) > 0: print(sym, flt)
+			frames[fld][sym] = series
 
-    results = defaultdict(pd.DataFrame)
-    for fld in frames:
-        print("Generating dataframe for {0} ...".format(fld))
-        results[fld] = pd.DataFrame(frames[fld])
+	results = defaultdict(pd.DataFrame)
+	for fld in frames:
+		print("Generating dataframe for {0} ...".format(fld))
+		results[fld] = pd.DataFrame(frames[fld])
 
-    if dump:
-        outputdir = fileName.split('.')[0]
-        if not os.path.exists(outputdir):
-            print("Creating directory {0} ...".format(outputdir))
-            os.system("mkdir -p {0}".format(outputdir))
+	if dump:
+		outputdir = fileName.split('.')[0]
+		if not os.path.exists(outputdir):
+			print("Creating directory {0} ...".format(outputdir))
+			os.system("mkdir -p {0}".format(outputdir))
 
-        for fld in results:
-            outputfile = os.path.join(outputdir, "{0}.csv".format(fld))
-            print("Generating file {0} ...".format(outputfile))
-            results[fld].to_csv(outputfile)
+		for fld in results:
+			outputfile = os.path.join(outputdir, "{0}.csv".format(fld))
+			print("Generating file {0} ...".format(outputfile))
+			results[fld].to_csv(outputfile)
 
-    return results
+	return results
 
 def loadDailyOpen(directory="crypto-markets"):
-    return loadDailyField(directory, "open")
+	return loadDailyField(directory, "open")
 
 def loadDailyHigh(directory="crypto-markets"):
-    return loadDailyField(directory, "high")
+	return loadDailyField(directory, "high")
 
 def loadDailyLow(directory="crypto-markets"):
-    return loadDailyField(directory, "low")
+	return loadDailyField(directory, "low")
 
 def loadDailyClose(directory="crypto-markets"):
-    return loadDailyField(directory, "close")
+	return loadDailyField(directory, "close")
 
 def loadDailyVolume(directory="crypto-markets"):
-    return loadDailyField(directory, "volume")
+	return loadDailyField(directory, "volume")
 
 def loadDailyMktCap(directory="crypto-markets"):
-    return loadDailyField(directory, "market")
+	return loadDailyField(directory, "market")
 
 def loadDailyField(directory, fld):
-    if not os.path.exists(directory):
-        print("Directory {0} doesn't exist!".format(directory))
-        return 
+	if not os.path.exists(directory):
+		print("Directory {0} doesn't exist!".format(directory))
+		return 
 
-    csvfile = os.path.join(directory, "{0}.csv".format(fld))
-    if not os.path.exists(csvfile):
-        print("CSV file {0} doesn't exist!".format(csvfile))
-        return
+	csvfile = os.path.join(directory, "{0}.csv".format(fld))
+	if not os.path.exists(csvfile):
+		print("CSV file {0} doesn't exist!".format(csvfile))
+		return
 
-    print("Loading {0} data from {1} ...".format(fld, csvfile))
-    res = pd.read_csv(csvfile, parse_dates=[0], index_col=[0])
-    return res
+	print("Loading {0} data from {1} ...".format(fld, csvfile))
+	res = pd.read_csv(csvfile, parse_dates=[0], index_col=[0])
+	return res
 
 
 if __name__ == "__main__":
-    pass
+	pass
 
-    """
-    res = processRawData()
-    dailyOpen   = loadDailyOpen()
-    dailyHigh   = loadDailyHigh()
-    dailyLow    = loadDailyLow()
-    dailyClose  = loadDailyClose()
-    dailyVolume = loadDailyVolume()
-    dailyMktCap = loadDailyMktCap()
-    """
+	"""
+	res = processRawData()
+	dailyOpen   = loadDailyOpen()
+	dailyHigh   = loadDailyHigh()
+	dailyLow    = loadDailyLow()
+	dailyClose  = loadDailyClose()
+	dailyVolume = loadDailyVolume()
+	dailyMktCap = loadDailyMktCap()
+	"""
 
-    btceur = load_minute_BTCEUR()
-    btcusd = load_minute_BTCUSD()
-    etheur = load_minute_ETHEUR()
-    ethusd = load_minute_ETHEUR()
-    ltcbtc = load_minute_LTCBTC()
-    ltceur = load_minute_LTCEUR()
-    ltcusd = load_minute_LTCUSD()
-    xrpusd = load_minute_XRPUSD()
+	btceur = load_minute_BTCEUR()
+	btcusd = load_minute_BTCUSD()
+	etheur = load_minute_ETHEUR()
+	ethusd = load_minute_ETHEUR()
+	ltcbtc = load_minute_LTCBTC()
+	ltceur = load_minute_LTCEUR()
+	ltcusd = load_minute_LTCUSD()
+	xrpusd = load_minute_XRPUSD()
 
-    cryptoDict = {
-        "BTCEUR" : btceur,
-        "BTCUSD" : btcusd,
-        "ETHEUR" : etheur,
-        "ETHUSD" : ethusd,
-        "LTCBTC" : ltcbtc,
-        "LTCEUR" : ltceur,
-        "LTCUSD" : ltcusd,
-        "XRPUSD" : xrpusd
-    }
+	cryptoDict = {
+		"BTCEUR" : btceur,
+		"BTCUSD" : btcusd,
+		"ETHEUR" : etheur,
+		"ETHUSD" : ethusd,
+		"LTCBTC" : ltcbtc,
+		"LTCEUR" : ltceur,
+		"LTCUSD" : ltcusd,
+		"XRPUSD" : xrpusd
+	}
 
-    mdata  = minuteOHLC(cryptoDict)
+	mdata  = minuteOHLC(cryptoDict)
--- signals.py
+++ signals.py
@@ -8,46 +8,46 @@ from data import loadDailyOpen, loadDailyHigh, loadDailyLow, loadDailyClose, loa
 from factors import crrank, universe, calcSD
 
 def cross_price_momentum(univ, close, N):
-    mom = np.log(close).diff(N)
-    mom[~univ] = np.nan
-    momRank = crrank(mom, 1, pct=True)
-    return momRank
+	mom = np.log(close).diff(N)
+	mom[~univ] = np.nan
+	momRank = crrank(mom, 1, pct=True)
+	return momRank
 
 def cross_price_momentum_adjust_by_vol(univ, close, vol, N):
-    mom = np.log(close).diff(N).div(vol)
-    mom[~univ] = np.nan
-    momRank = crrank(mom, 1, pct=True)
-    return momRank
+	mom = np.log(close).diff(N).div(vol)
+	mom[~univ] = np.nan
+	momRank = crrank(mom, 1, pct=True)
+	return momRank
 
 
 if __name__ == "__main__":
 
-    dailyOpen   = loadDailyOpen()
-    dailyHigh   = loadDailyHigh()
-    dailyLow    = loadDailyLow()
-    dailyClose  = loadDailyClose()
-    dailyVolume = loadDailyVolume()
-    dailyMktCap = loadDailyMktCap()
+	dailyOpen   = loadDailyOpen()
+	dailyHigh   = loadDailyHigh()
+	dailyLow    = loadDailyLow()
+	dailyClose  = loadDailyClose()
+	dailyVolume = loadDailyVolume()
+	dailyMktCap = loadDailyMktCap()
 
-    atr = loadDailyField("crypto-markets", "atr2")
+	atr = loadDailyField("crypto-markets", "atr2")
 
-    mktCapRank  = crrank(dailyMktCap)
-    volumeRank  = crrank(dailyVolume)
+	mktCapRank  = crrank(dailyMktCap)
+	volumeRank  = crrank(dailyVolume)
 
 
-    univ = universe(mktCapRank, dailyVolume)
+	univ = universe(mktCapRank, dailyVolume)
 
-    priceMom5D   = cross_price_momentum(univ, dailyClose, 5)
-    priceMom10D  = cross_price_momentum(univ, dailyClose, 10)
-    priceMom15D  = cross_price_momentum(univ, dailyClose, 15)
-    priceMom25D  = cross_price_momentum(univ, dailyClose, 25)
+	priceMom5D   = cross_price_momentum(univ, dailyClose, 5)
+	priceMom10D  = cross_price_momentum(univ, dailyClose, 10)
+	priceMom15D  = cross_price_momentum(univ, dailyClose, 15)
+	priceMom25D  = cross_price_momentum(univ, dailyClose, 25)
 
-    mktCapMom5D  = cross_price_momentum(univ, dailyMktCap, 5)
-    mktCapMom10D = cross_price_momentum(univ, dailyMktCap, 10)
-    mktCapMom15D = cross_price_momentum(univ, dailyMktCap, 15)
-    mktCapMom25D = cross_price_momentum(univ, dailyMktCap, 25)
+	mktCapMom5D  = cross_price_momentum(univ, dailyMktCap, 5)
+	mktCapMom10D = cross_price_momentum(univ, dailyMktCap, 10)
+	mktCapMom15D = cross_price_momentum(univ, dailyMktCap, 15)
+	mktCapMom25D = cross_price_momentum(univ, dailyMktCap, 25)
 
-    adjMktCapMom5D  = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 5)
-    adjMktCapMom10D = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 10)
-    adjMktCapMom15D = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 15)
-    adjMktCapMom25D = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 25)
+	adjMktCapMom5D  = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 5)
+	adjMktCapMom10D = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 10)
+	adjMktCapMom15D = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 15)
+	adjMktCapMom25D = cross_price_momentum_adjust_by_vol(univ, dailyMktCap, atr, 25)
